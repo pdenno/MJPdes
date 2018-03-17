333a117970079f4de18fa9a3edfb376d3ef01f6f
Peter Denno <podenno@gmail.com>
2018-03-17 15:14:49 -0400

    Updates for parallel workstations; seems to be working

--- src/gov/nist/MJPdes/core.clj
+++ src/gov/nist/MJPdes/core.clj
@@ -859,28 +859,3 @@
    Atom the-des-model gets updated in the process."
   [continuous-model n]
   (repeatedly n #(<!! (:log-chan continuous-model))))
-
-(def parallel
-   (map->Model 
-    {:line     
-     {:m1 (map->ExpoMachine {:lambda 0.1 :mu 0.9 :W 1.0 })  
-      :b1 (map->Buffer {:N 3})                              
-      :m2 (map->ExpoMachine {:lambda 0.1 :mu 0.9 :W 1.0 })
-      :b2 (map->Buffer {:N 5})
-      :m3-1 (map->ExpoMachine {:lambda 0.1 :mu 0.9 :W 1.0 })
-      :m3-2 (map->ExpoMachine {:lambda 0.1 :mu 0.9 :W 1.0 })
-      :m3-3 (map->ExpoMachine {:lambda 0.1 :mu 0.9 :W 1.0 }) 
-      :b3 (map->Buffer {:N 1})                              
-      :m4 (map->ExpoMachine {:lambda 0.1 :mu 0.9 :W 1.0 }) 
-      :b4 (map->Buffer {:N 1})
-      :m5 (map->ExpoMachine {:lambda 0.1 :mu 0.9 :W 1.0 })}
-     :topology [:m1 :b1 :m2 :b2
-                {:type :parallel-or :name :m3 :machines [:m3-1 :m3-2 :m3-3]}
-                :b3 :m4 :b4 :m5]
-     :entry-point :m1
-     :report {:log? true :max-lines 200}
-     :params {:warm-up-time 200 :run-to-time 2000}
-     :jobmix {:jobType1 (map->JobType {:portion 0.8
-                                       :w {:m1 1.0, :m2 1.0, :m3 3.0, :m4 1.0, :m5 1.0}})
-              :jobType2 (map->JobType {:portion 0.2
-                                       :w {:m1 1.0, :m2 1.0, :m3 3.6, :m4 1.0, :m5 1.0}})}}))
